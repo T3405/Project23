719454b945da905a4d522614062076a7452034f2
cl3t4p <61201782+cl3t4p@users.noreply.github.com>
2023-05-15 13:27:06 +0200

    Completed f4_play

--- F4Server/src/f4logic.c
+++ F4Server/src/f4logic.c
@@ -1,5 +1,59 @@
 #include "f4logic.h"
 
+
+int check_winner(pid_t** matrix, pid_t player, int row_size, int column_size) {
+    // Check horizontally
+    for (int row = 0; row < row_size; row++) {
+        for (int col = 0; col < column_size - 3; col++) {
+            if (matrix[row][col] == player &&
+                matrix[row][col + 1] == player &&
+                matrix[row][col + 2] == player &&
+                matrix[row][col + 3] == player) {
+                return 1; // Player wins horizontally
+            }
+        }
+    }
+
+    // Check vertically
+    for (int row = 0; row < row_size - 3; row++) {
+        for (int col = 0; col < column_size; col++) {
+            if (matrix[row][col] == player &&
+                matrix[row + 1][col] == player &&
+                matrix[row + 2][col] == player &&
+                matrix[row + 3][col] == player) {
+                return 1; // Player wins vertically
+            }
+        }
+    }
+
+    // Check diagonally (top-left to bottom-right)
+    for (int row = 0; row < row_size - 3; row++) {
+        for (int col = 0; col < column_size - 3; col++) {
+            if (matrix[row][col] == player &&
+                matrix[row + 1][col + 1] == player &&
+                matrix[row + 2][col + 2] == player &&
+                matrix[row + 3][col + 3] == player) {
+                return 1; // Player wins diagonally (top-left to bottom-right)
+            }
+        }
+    }
+
+    // Check diagonally (top-right to bottom-left)
+    for (int row = 0; row < row_size - 3; row++) {
+        for (int col = 3; col < column_size; col++) {
+            if (matrix[row][col] == player &&
+                matrix[row + 1][col - 1] == player &&
+                matrix[row + 2][col - 2] == player &&
+                matrix[row + 3][col - 3] == player) {
+                return 1; // Player wins diagonally (top-right to bottom-left)
+            }
+        }
+    }
+
+    return 0; // Player does not win
+}
+
+
 /*
  * Funzione modifica matrice e aggiunge il player
  * Controlla se e una giocata valida ritrona -1
@@ -9,12 +63,27 @@
  * TODO save matrix
  */
 int f4_play(pid_t** matrix,int column,pid_t player,int row_size,int column_size){
+    //Controlla se la colonna esiste
     if(column >= column_size){
         return -1;
     }
 
-    return 0;
+    //Controlla che ci sia spazio sulla colonn
+    char is_max_col = 0;
+    int i;
+    for (i = 0; i < column_size; ++i){
+        if(matrix[column][i] == 0){
+            matrix[column][i] = player;
+            is_max_col = 1;
+            break;
+        }
+    }
+    if(!is_max_col){
+        return -1;
+    }
+    return check_winner(matrix,player,row_size,column_size);
 }
+
 
 void clean_array(pid_t ** matrix,int row_size,int column_size){
     for (int i = 0; i < row_size; ++i)
