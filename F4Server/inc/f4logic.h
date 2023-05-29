2c238c98a032df60e9af6709431ce1379a44b99e
cl3t4p <61201782+cl3t4p@users.noreply.github.com>
2023-05-29 17:19:27 +0200

    fixed winner horizontal and vertical

--- F4Server/inc/f4logic.h
+++ F4Server/inc/f4logic.h
@@ -1,27 +1,31 @@
 #include <unistd.h>
 
 /**
+ *
  * Use a array as a matrix
- * V = matrix
- * X = column size
- * y = row
- * z = column
+ * @param V = matrix
+ * @param X = column_size = rows
+ * @param Y = n_row
+ * @param Z = n_column
  */
-#define GET_M(V, X, Y, Z) (V[Y*X+Z])
+#define GET_M(V, X, Y, Z) (V[Z*X+Y])
+
+#define PRINT_M(X, Y, Z) printf("Row : %d , Column : %d\nIndex : %d\n",Y,Z,(Z*X+Y));
+
 
 /**
  * Play the action and check if there is a winner
  * @param matrix matrix to play on
  * @param column action
  * @param player pid_t of the player
- * @param row_size row size of the matrix
- * @param column_size column size of the matrix
+ * @param n_column n_row size of the matrix
+ * @param n_row n_column size of the matrix
  * @return -1 if there is a invalid plays,0 normal play,pid_t the pid of the winner
  */
-int f4_play(pid_t* matrix,int column,pid_t player,int row_size,int column_size);
+int f4_play(pid_t* matrix, int column, pid_t player, int n_column, int n_row);
 
 /**
  * Fill the array all with 0
  * @param matrix the matrix to fill
  */
-void clean_array(pid_t* matrix,int row_size,int column_size);
+void clean_array(pid_t* matrix, int n_row, int n_column);
