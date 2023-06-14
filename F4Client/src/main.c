4e5f9d6de748bf00d7e28e985d32e0c180811f3c
cl3t4p <61201782+cl3t4p@users.noreply.github.com>
2023-06-14 10:38:04 +0200

    formatted

--- F4Client/src/main.c
+++ F4Client/src/main.c
@@ -18,159 +18,167 @@
 char status = 1;
 
 
-
 //funzione per il secondo segnale d'uscita ctrl+c
-void signalHandler(int signal) {
+void signal_close(int signal) {
     status = 0;
-    //Mandare signal abbandono
 }
 
 //funzione per il primo segnale d'uscita ctrl+c
-void alertHandler(int sig){
+void signal_alert(int sig) {
     printf("Sei sicuro di uscire se si ripremi ctrl + c\n");
-    signal(SIGINT, signalHandler); // Primo ctrl +c
+    signal(SIGINT, signal_close); // Primo ctrl +c
 }
 
 
-int main(int argc,char* argv[]) {
+int main(int argc, char *argv[]) {
 
-    signal(SIGINT, alertHandler); // Primo ctrl +c
+    signal(SIGINT, signal_alert);
 
-    int fd = open(DEFAULT_PATH,O_WRONLY);
+    //Open default path for fifo
+    int fd = open(DEFAULT_PATH, O_WRONLY);
     if (access(DEFAULT_PATH, W_OK) != 0) {
         printf("Please start the server first!\n");
         exit(0);
     }
+
     struct client_info clientInfo;
     //Set pid
     clientInfo.pid = getpid();
     //Set check if mode is single player
     clientInfo.mode = '\0';
-    if(argc > 1){
-        if(argv[1][0] == '*'){
+    if (argc > 1) {
+        if (argv[1][0] == '*') {
             clientInfo.mode = argv[1][0];
-             //modalita' single player
+            //modalita' single player
         }
     }
     //Write to FIFO and connect to client
-    write(fd,&clientInfo, sizeof(struct client_info));
+    write(fd, &clientInfo, sizeof(struct client_info));
 
+    //Wait for the FIFO file to be created at /tmp/f4/<pid>
     char path[1024];
-    sprintf(path,"%s%d",DEFAULT_DIR,clientInfo.pid);
-    while (access(path, F_OK) != 0) {
-        //Wait till the file exists
-    }
+    sprintf(path, "%s%d", DEFAULT_DIR, clientInfo.pid);
+    while (access(path, F_OK) != 0);
     errno = 0;
-    printf("path : %s\n",path);
+
+    //printf("path : %s\n",path);
 
 
 
-    int input_fd = open(path,O_RDONLY);
+    //Open the input_fifo
+    int input_fd = open(path, O_RDONLY);
     //Wait for symbol
 
+
+    //Read its own symbol
     char symbol[2];
     int code;
     code = cmd_read_code(input_fd);
-    read(input_fd,&symbol[0], sizeof(char));
-    printf("code %d,char %c\n",code,symbol[0]);
+    read(input_fd, &symbol[0], sizeof(char));
+    printf("code %d,char %c\n", code, symbol[0]);
 
+    //Read opponent symbol
     code = cmd_read_code(input_fd);
-    read(input_fd,&symbol[1], sizeof(char));
-    printf("code %d,char %c\n",code,symbol[1]);
+    read(input_fd, &symbol[1], sizeof(char));
+    printf("code %d,char %c\n", code, symbol[1]);
 
 
+    //Read msg_qq key_t
     key_t output_qq_id;
     code = cmd_read_code(input_fd);
-    read(input_fd, &output_qq_id, sizeof (output_qq_id));
+    read(input_fd, &output_qq_id, sizeof(output_qq_id));
     printf("code %d ,key %d\n", code, output_qq_id);
 
+    //Read shared_mem_info (row,column,key_t shared_mem)
     struct shared_mem_info shared_mem_info;
     code = cmd_read_code(input_fd);
-    read(input_fd,&shared_mem_info, sizeof(shared_mem_info));
-    printf("shared_key %d\n",shared_mem_info.key);
+    read(input_fd, &shared_mem_info, sizeof(shared_mem_info));
+    printf("shared_key %d\n", shared_mem_info.key);
 
 
+    //Attach shared-memory
     int mem_id = shmget(shared_mem_info.key, shared_mem_info.column * shared_mem_info.row * sizeof(pid_t), 0666);
-    pid_t* board = shmat(mem_id, NULL, O_RDONLY);
+    pid_t *board = shmat(mem_id, NULL, O_RDONLY);
 
-    int output_qq = msgget(output_qq_id,0666);
+    //Open msg_qq
+    int output_qq = msgget(output_qq_id, 0666);
 
 
-    //Set the fd with a O_NON_BLOCK
-    fcntl(STDIN_FILENO,F_SETFL, fcntl(STDIN_FILENO,F_GETFL)|O_NONBLOCK);
-    fcntl(input_fd,F_SETFL, fcntl(input_fd,F_GETFL)|O_NONBLOCK);
+    //Set the both fd with a O_NON_BLOCK
+    fcntl(STDIN_FILENO, F_SETFL, fcntl(STDIN_FILENO, F_GETFL) | O_NONBLOCK);
+    fcntl(input_fd, F_SETFL, fcntl(input_fd, F_GETFL) | O_NONBLOCK);
 
-    int row = shared_mem_info.row;
-    int column = shared_mem_info.column;
+    unsigned int row = shared_mem_info.row;
+    unsigned int column = shared_mem_info.column;
     char turn = 0;
 
-    while (status){
-        code = cmd_read_code(input_fd);
-            switch (code) {
-                case 0:
-                    break;
-                //Server set sem 2
-                case CMD_UPDATE: {
-                    for (int i = 0; i < row; ++i) {
-                        for (int j = 0; j < column; ++j) {
-                            pid_t player_id = GET_M(board,row,i,j);
-                            char print_symbol;
-                            if(player_id == getpid()){
-                                print_symbol = symbol[0];
-                            }else if (player_id == 0){
-                                print_symbol = ' ';
-                            }else{
-                                print_symbol = symbol[1];
-                            }
-                            printf("|%c", print_symbol);
-                        }
-                        printf("|\n");
-                    }
 
-                    //sem--
-                    break;
+    while (status) {
+        code = cmd_read_code(input_fd);
+        switch (code) {
+            case 0:
+                break;
+                //Server set sem 2
+            case CMD_UPDATE: {
+                for (int i = 0; i < row; ++i) {
+                    for (int j = 0; j < column; ++j) {
+                        pid_t player_id = GET_M(board, row, i, j);
+                        char print_symbol;
+                        if (player_id == getpid()) {
+                            print_symbol = symbol[0];
+                        } else if (player_id == 0) {
+                            print_symbol = ' ';
+                        } else {
+                            print_symbol = symbol[1];
+                        }
+                        printf("|%c", print_symbol);
+                    }
+                    printf("|\n");
                 }
-                case CMD_TURN: {
-                    turn = 1;
+                break;
+            }
+            case CMD_TURN: {
+                turn = 1;
+                printf("It's your turn\n");
+                break;
+            }
+            case CMD_INPUT_ERROR: {
+                int error;
+                read(input_fd, &error, sizeof(int));
+                if (error == 0) {
+                    //Wrong input input
                     printf("It's your turn\n");
-                    break;
-                }
-                case CMD_INPUT_ERROR: {
-                    int error;
-                    read(input_fd, &error, sizeof(int));
-                    if (error == 0) {
-                        //Wrong input input
-                        printf("It's your turn\n");
-                        turn = 1;
-                    } else if (error == 1) {
-                        printf("It's not your turn\n");
-                        //Print not your turn
-                    }
-                }
-                    break;
-                case CMD_WINNER: {
-                    char winner;
-                    read(input_fd,&winner, sizeof(char));
-                    printf("winner : %c\n",winner);
-                    if(winner == symbol[0]) {
-                        printf("You are the winner!\n");
-                        break;
-                        //You win
-                    }else {
-                        printf("You lost!\n");
-                        break;
-                    }
+                    turn = 1;
+                } else if (error == 1) {
+                    printf("It's not your turn\n");
+                    //Print not your turn
                 }
             }
-            char input_char[20];
-            if(read(STDIN_FILENO,input_char, 20) > 0){
-                struct client_msg msg;
-                msg.mtype = 1;
-                msg.pid = getpid();
-                //-1 because the board start at 1
-                msg.move = atoi(input_char)-1;
-                msgsnd(output_qq,&msg, sizeof(msg)- sizeof(long),0);
+                break;
+            case CMD_WINNER: {
+                char winner;
+                read(input_fd, &winner, sizeof(char));
+                printf("winner : %c\n", winner);
+                if (winner == symbol[0]) {
+                    printf("You are the winner!\n");
+                    break;
+                    //You win
+                } else {
+                    printf("You lost!\n");
+                    break;
+                }
             }
+        }
+        char input_char[20];
+        if (read(STDIN_FILENO, input_char, 20) > 0) {
+            struct client_msg msg;
+            msg.mtype = 1;
+            msg.pid = getpid();
+            //-1 because the board start at 1
+            msg.move = atoi(input_char) - 1;
+
+            msgsnd(output_qq, &msg, sizeof(msg) - sizeof(long), 0);
+        }
 
 
     }
