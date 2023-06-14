b8ad9bcceaa220af20378c2dee7deb24a93d724e
cl3t4p <61201782+cl3t4p@users.noreply.github.com>
2023-06-14 18:10:47 +0200

    update using signal

--- F4Client/src/client_cmd.c
+++ F4Client/src/client_cmd.c
@@ -1,14 +1,8 @@
 #include "client_cmd.h"
 #include "commands.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 int cmd_read_code(int fd){
     int n = 0;
     read(fd,&n,sizeof(int));
-      if(n == CMD_SERVER_OFFLINE){
-        printf("The server is offline!\n");
-        exit(0);
-      }
     return n;
 }
